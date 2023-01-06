d0d165e6c2565802fc2175246a6fa8e4be157764
l8rgt <38414254+l8rgt@users.noreply.github.com>
2023-01-06 17:08:48 -0500

    rewrote the output message
     Rewrote the output message to allow for a more detailed embed and use of the twitch API

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-Add "index.py" file with variable "clientAuth" equal to your token to use the bot. Make sure to update the message and channel in "v2.py".
+create a new file in this directory called "index.py" with the contents below:
+```
+clientAuth = ''                                          #discord bot client ID
+TwitchID = ''                                            #Twitch Client ID
+TwitchSecret = ''                                        #Twitch Client Secret
+ChannelName = ''                                         #Twitch Channel Name
+DiscordChannelID =                                       #Discord Channel ID
+DiscordServerID =                                        #Discord Server ID
+```
+Fill in the spaces as required. (inside the '' unless there are no '' present)
 If needed, update prefix in "prefix.json"
+To edit the output message, go to line 62 on "v2.py"
 
 Note: the program calls "v2.py" instead of having the message in the cog in order to make it easier to call the command via a webhook if the user does not want to use the command
+
+
+- Discord bot ID location: https://discord.com/developers/applications
+- Twitch API IDs location: https://dev.twitch.tv/console/apps
+- To get the discord channel and server IDs enable developer mode on your client
--- v2.py
+++ v2.py
@@ -4,18 +4,64 @@ from discord.ext import commands
 import json
 intent = discord.Intents.all()
 import sys
+import requests
 
-from index import clientAuth                                               # in this directory create an index.py file with the variable "clientAuth"
+from index import clientAuth                                              
+from index import TwitchID
+from index import TwitchSecret
+from index import ChannelName
+from index import DiscordChannelID
+from index import DiscordServerID
+
+# Get OAuth token from Twitch
+response = requests.post('https://id.twitch.tv/oauth2/token', data={
+    'client_id': TwitchID,
+    'client_secret': TwitchSecret,
+    'grant_type': 'client_credentials'
+})
+
+response_json = response.json()
+access_token = response_json['access_token']
+headers = {
+    'Authorization': f'Bearer {access_token}',
+    'Client-ID': TwitchID
+}
+
+#Get some channel data including channel ID
+getuser = requests.get('https://api.twitch.tv/helix/users', params={
+    'login': ChannelName
+}, headers=headers)
+
+getUser_json = getuser.json()
+user = getUser_json['data'][0]
+profilePic=user['profile_image_url']    
+displayName=user['display_name']
+channelID = user['id']
+
+#Get the stream title and game
+getTitle = requests.get('https://api.twitch.tv/helix/channels', params={
+    'broadcaster_id': channelID
+}, headers=headers)
+
+getTitle_json = getTitle.json()
+streamData = getTitle_json['data'][0]
+streamTitle=streamData['title']
+streamGame = streamData['game_name']
+
 
 pref=json.load(open('prefix.json','r'))
 bot = commands.Bot(command_prefix=pref,help_command=None,intents=intent)
 
 @bot.event
 async def on_ready():
-    guild=bot.get_guild(413457661988962304)#insert server
-    channel = guild.get_channel(678030513906253863)#insert id here
-    await channel.send("Hey @everyone our official twitch FSC_Esports is now live at https://twitch.tv/FSC_esports ! Come support us! Go Rams!")
-    #await channel.send("test")
+    guild=bot.get_guild(DiscordServerID)
+    channel = guild.get_channel(DiscordChannelID)
+    embedVar = discord.Embed(title=streamTitle, url="https://twitch.tv/"+ChannelName, color = 0x6441A4)
+    embedVar.set_author(name=displayName, icon_url=profilePic)
+    embedVar.set_image(url="https://static-cdn.jtvnw.net/previews-ttv/live_user_"+ChannelName+"-320x180.jpg?r=834905")
+    embedVar.add_field(name="Game:", value=streamGame)
+    embedVar.set_thumbnail(url=profilePic)
+    await channel.send("Hey @everyone our official twitch "+displayName+" is now live at <https://twitch.tv/"+ChannelName+">! Come support us! Go Rams!",embed=embedVar)  #For a blank message above the embed, replace the first string with ""
     sys.exit(0)
 
 bot.run(clientAuth)
